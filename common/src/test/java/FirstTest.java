17a7e7f77a0a152fae977fc7494d2df15b0666ca
wds1993225 <wds1993225@126.com>
2018-04-25 12:05:43 +0800

    init

---
+++ common/src/test/java/FirstTest.java
@@ -0,0 +1,13 @@
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by moon on 2018/4/25 .
+ */
+@Slf4j
+public class FirstTest {
+
+    public static void main(String[] args) {
+        TestData data = new TestData();
+        log.debug("haha");
+    }
+}
---
+++ common/src/test/java/TestData.java
@@ -0,0 +1,10 @@
+import lombok.Data;
+
+/**
+ * Created by moon on 2018/4/25 .
+ */
+@Data
+public class TestData {
+    private String name;
+    private int age;
+}
